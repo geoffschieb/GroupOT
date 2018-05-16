7b067040494d4d0a644e03c8a29f6031dc84ddd4
Aden Forrow <aforrow@mit.edu>
2018-05-16 12:07:54 -0400

    ValueError

--- mnist.py
+++ mnist.py
@@ -10,7 +10,7 @@ np.random.seed(1)
 
 
 
-num_clean_samples = 6000 # number of images in dataset without noise
+num_clean_samples = 1000 # number of images in dataset without noise
 num_noisy_samples = num_clean_samples
 
 clean_data = mnist.train.next_batch(num_clean_samples)
@@ -21,24 +21,24 @@ dimensions = clean_data[0].shape[1]
 
 # Prepare noisy_data by adding Gaussian noise at different levels
 
-alphas = np.linspace(0,1,6)
+alphas = np.array([0.0,0.2])#np.linspace(0,1,6)
 
 noisy_data = [((1-alpha)*future_noisy_data[0] + alpha* np.random.randn(num_noisy_samples, dimensions), future_noisy_data[1], alpha) for alpha in alphas]
 
 
 # Classify at each noise level using clean data
 
-samples_test = 5
+samples_test = 1
 samples_train = samples_test
-entr_regs = np.array([10.0])**(-3,5)
-gl_params = np.array([10.0])**(-3,5)
-ks =  np.array([2])**range(1,8)
+entr_regs = np.array([10.0])**range(-3,5)
+gl_params = np.array([10.0])**range(-3,5)
+ks =  np.array([10, 20, 30, 40, 50, 60, 70, 80])
 
 estimators = {
-                "ot_gl": {
-                                    "function": "ot_gl",
-                                    "parameter_ranges": [entr_regs, gl_params]
-                                    },
+    #"ot_gl": {
+    #                               "function": "ot_gl",
+    #                               "parameter_ranges": [entr_regs, gl_params]
+    #                               },
                 "ot": {
                                     "function": "ot",
                                     "parameter_ranges": []
@@ -47,10 +47,10 @@ estimators = {
                                     "function": "ot_entr",
                                     "parameter_ranges": [entr_regs]
                                     },
-                "ot_kmeans": {
-                                    "function": "ot_kmeans",
-                                    "parameter_ranges": [entr_regs, ks]
-                                    },
+                                    #"ot_kmeans": {
+                                    #"function": "ot_kmeans",
+                                    #"parameter_ranges": [entr_regs, ks]
+#},
                 "ot_2kbary": {
                                     "function": "ot_2kbary",
                                     "parameter_ranges": [entr_regs, ks]
@@ -59,22 +59,22 @@ estimators = {
                                     "function": "ot_kbary",
                                     "parameter_ranges": [entr_regs, ks]
                                 },
-                "noadj": {
-                                    "function": "noadj",
-                                    "parameter_ranges": []
-                                    },
-                "sa": {
-                                    "function": "sa",
-                                    "parameter_ranges": [ks]
-                                    },
-                "tca": {
-                                    "function": "tca",
-                                    "parameter_ranges": [ks]
-                                    },
-                "coral": {
-                                    "function": "coral",
-                                    "parameter_ranges": []
-                                    }
+                                    #"noadj": {
+                                    #"function": "noadj",
+                                    #"parameter_ranges": []
+                                    #},
+                #"sa": {
+                #                    "function": "sa",
+                               #                    "parameter_ranges": [ks]
+#                    },
+                # "tca": {
+                #                   "function": "tca",
+                #                   "parameter_ranges": [ks]
+#                                },
+                                    #"coral": {
+                                    #"function": "coral",
+                                    #"parameter_ranges": []
+#}
                 } 
 
 sim_params = {
@@ -92,7 +92,7 @@ for target in noisy_data:
 
     print("Noise level: alpha = {}".format(target[2]))
     def get_data(train, i):
-        num_samples = 500
+        num_samples = 90
         start_index = i*num_samples*2
         if train:
             start_index = start_index + num_samples
